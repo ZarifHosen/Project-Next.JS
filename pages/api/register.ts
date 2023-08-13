1c9884f069509f6fb36b306f7bc99fed13d065f3
Zarif Hosen <87227163+ZarifHosen@users.noreply.github.com>
2023-08-13 12:04:55 +0000

    signin  api added

--- pages/api/register.ts
+++ pages/api/register.ts
@@ -1,16 +1,31 @@
-import { hashPassword } from 'libs/auth'
-import {NextApiRequest, NextApiResponse} from 'next'
+import { createJWT, hashPassword } from "libs/auth";
+import { NextApiRequest, NextApiResponse } from "next";
+import { db } from "libs/db";
+import { serialize } from "cookie";
 
-
-export default async function register(req: NextApiRequest, res: NextApiResponse) {
-
-    if (req.method === 'POST') {
+export default async function register(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
     const user = await db.user.create({
-        data: {
-            email: req.body.email,
-            password: await hashPassword(req.body.password),
-            firstName: req.body.firstName,
-            lastName: req.body.lastName
-        }
-    })
+      data: {
+        email: req.body.email,
+        password: await hashPassword(req.body.password),
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+      },
+    });
+    const jwt = await createJWT(user);
+
+    res.setHeader(
+      "Set-Cookie",
+      serialize(process.env.COOKIE_NAME, jwt, {
+        httpOnly: true,
+        path: "/",
+        maxAge: 60 * 60 * 24 * 7,
+      })
+    );
+  }
+  res.status(201).end();
 }
---
+++ pages/api/signin.ts
@@ -0,0 +1,37 @@
+import { serialize } from "cookie";
+import { comparePassword, createJWT } from "libs/auth";
+import { db } from "libs/db";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async function signin(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method === "POST") {
+    const user = await db.user.findUnique({
+      where: {
+        email: req.body.email,
+      },
+    });
+
+    const isUser = await comparePassword(req.body.password, user?.password);
+
+    if (isUser) {
+      const jwt = await createJWT(user);
+
+      res.setHeader(
+        "Set-Cookie",
+        serialize(process.env.COOKIE_NAME, jwt, {
+          httpOnly: true,
+          path: "/",
+          maxAge: 60 * 60 * 24 * 7,
+        })
+      );
+      res.status(201);
+      res.end();
+    } else {
+      res.status(401);
+      res.json({ error: "Invalid login" });
+    }
+  }
+}
