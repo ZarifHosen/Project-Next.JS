fb20a7dd4f4070419ea6784284141a753ae6b790
Zarif <zarifhosen2015@gmail.com>
2023-08-14 21:06:50 +0600

    error solved

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -1,18 +1,17 @@
-import React from "react";
-import { Grid, Calendar, User, Settings } from "react-feather"; // Import icons
 import Card from "./Card";
 import SidebarLink from "./SidebarLink";
+
 const links = [
-  { label: "Home", icon: Grid, link: "/home" },
+  { label: "Home", icon: "Grid", link: "/home" },
   {
     label: "Calendar",
-    icon: Calendar,
+    icon: "Calendar",
     link: "/calendar",
   },
-  { label: "Profile", icon: User, link: "/profile" },
+  { label: "Profile", icon: "User", link: "/profile" },
   {
     label: "Settings",
-    icon: Settings,
+    icon: "Settings",
     link: "/settings",
   },
 ];
@@ -21,7 +20,7 @@ const Sidebar = () => {
   return (
     <Card className="h-full w-40 flex items-center justify-between flex-wrap">
       {links.map((link) => (
-        <SidebarLink key={link.label} link={link} /> // Added key prop
+        <SidebarLink key={link.label} link={link} />
       ))}
     </Card>
   );
